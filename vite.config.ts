25c1b9e3f424ec04d47d17f7deba313b3fe7d49d
Jeroen Smits <beaker73@live.nl>
2024-05-10 23:07:25 +0200

    adjust base path for github.io

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/satisfactory-designer", // path on github.io
 	resolve: {
 		alias: [
 			{ find: "@", replacement: path.resolve(__dirname, "src") },
